cb0d911f57f78d23c252a66712c5090b238bd27f
mezoncecile <mezoncecile@gmail.com>
2019-11-25 14:25:39 +0100

    géneration Model User avec Devise, + génération des views basiques de devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # J'ai suivi le cours, cette ligne est obligatoire pour ActionMailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
