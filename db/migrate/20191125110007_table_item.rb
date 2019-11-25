3e3d0377684868d5bc9e76a5e540895ffdd562b0
mezoncecile <mezoncecile@gmail.com>
2019-11-25 15:43:56 +0100

    table item + table user merged OK, seed fonctionnel

--- db/migrate/20191125110007_table_item.rb
+++ db/migrate/20191125110007_table_item.rb
@@ -1,10 +1,11 @@
 class TableItem < ActiveRecord::Migration[5.2]
   def change
-  	create_table :item
+  	create_table :item do |t|
   	 t.string :title
   	 t.text :description
   	 t.decimal :price
   	 t.string :image_url
+  	end
   end
 end
 
@@ -13,4 +14,3 @@ end
 #Pour le moment, l'image de l'item sera un string 
 #qui redirigera vers une url directe d'une image. 
 #Pour l'appeler, tu pourras faire : 
- #   <img src="<%= @item.image_url %>"> #
--- db/migrate/20191125125017_table_items.rb
+++ db/migrate/20191125125017_table_items.rb
@@ -1,10 +1,13 @@
 class TableItems < ActiveRecord::Migration[5.2]
   def change
-  	create_table :item
+  	create_table :items do |t|
   	 t.string :title
   	 t.text :description
   	 t.decimal :price
   	 t.string :image_url
+
+  	 t.timestamps
+  	end
   end
 end
 
@@ -13,4 +16,3 @@ end
 #Pour le moment, l'image de l'item sera un string 
 #qui redirigera vers une url directe d'une image. 
 #Pour l'appeler, tu pourras faire : 
- #  DANS LA VIEW <img src="<%= @item.image_url %>"> #
