4cf572bd21697d40669741a7bdbdfcbaf8e0daa3
hunaba <abadeer.hunson@yandex.ru>
2019-11-26 22:01:04 +0100

    modif table order db/migrate

--- db/migrate/20191126102033_create_orders.rb
+++ db/migrate/20191126102033_create_orders.rb
@@ -1,10 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
-    create_table :orders do |t|
+    	create_table :orders do |t|
 
     	t.belongs_to :user, index: true #reference à user
-
-      t.timestamps
+    	t.belongs_to :item, index: true 
+      	t.timestamps
     end
   end
 end
